b9e785e04a320fab574903c8f07effa7087b7a70
atifkhalil <atifkhalil520@gmail.com>
2025-02-22 00:58:57 +0000

    First Commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // Ensure root is the project folder
+  build: {
+    outDir: 'dist', // Output directory for the build
+    emptyOutDir: true, // Ensure it's cleared before building
+  },
 })
+
+
